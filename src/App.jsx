48d16767b4617a28cbb5b54bfd96d42a6aa2de69
patel <neelp060@gmail.com>
2023-04-18 18:47:36 -0400

    added the react function componentidmount and in that function fetching an API to diplay robots

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import "./App.css";
 
-const App = () => {
-return(
-    <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />;
-  </div>);
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filterRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    // if the server is slow to provide the info the conitional statement below will display a loading message on the browser. Else will reder the robots.
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox onSearchChange={this.onSearchChange} />
+          <CardList robots={filterRobots} />;
+        </div>
+      );
+    }
+  }
+}
 
 export default App;
